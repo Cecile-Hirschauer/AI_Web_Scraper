5b8bf9910bbf46b7f41b8f0d894537be93760246
Cécile Hirschauer <cecile.hirschauer@bbox.fr>
2025-03-21 14:04:17 +0100

    Issue with chromedriver

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
+from scraper import scrape_website
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter the URL of the website you want to scrape:")
 
 if st.button("Scrape Website"):
     st.write(f"Scraping website")
-
+    result = scrape_website(url)
+    print(result)
 
 
--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import time
 def scrape_website(url):
     print("Launching Chrome browser...")
 
-    chrome_driver_path = "/usr/local/bin/chromedriver"
+    chrome_driver_path = "./chromedriver/chromedriver"
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
@@ -15,8 +15,6 @@ def scrape_website(url):
         html = driver.page_source
         time.sleep(10)
         return html
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+
     finally:
         driver.quit()
