644f990e7f84cbaf1e39f65322c7628a1f423df3
Cécile Hirschauer <cecile.hirschauer@bbox.fr>
2025-03-21 23:06:30 +0100

    Clean DOM content

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import streamlit as st
-from scraper import scrape_website
+from scrape import scrape_website, extract_body_content, clean_body_content, split_dom_content
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter the URL of the website you want to scrape:")
 
+# Step 1: Scrape the Website
 if st.button("Scrape Website"):
-    st.write(f"Scraping website")
-    result = scrape_website(url)
-    print(result)
+    if url:
+        st.write("Scraping the website...")
 
+        # Scrape the website
+        dom_content = scrape_website(url)
+        body_content = extract_body_content(dom_content)
+        cleaned_content = clean_body_content(body_content)
 
+        # Store the DOM content in Streamlit session state
+        st.session_state.dom_content = cleaned_content
+
+        # Display the DOM content in an expandable text box
+        with st.expander("View DOM Content"):
+            st.text_area("DOM Content", cleaned_content, height=300)
+
+# Step 2: Ask Questions About the DOM Content
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse")
